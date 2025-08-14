d1b104cc2e778b09168e5f93efc780f58dc5a0a6
Sebastian Vinay Muñoz Diaz <sebasvinaym@outlook.com>
2025-08-14 18:45:12 -0500

    changing playwright config endpoint

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:8080',
-    // baseURL: 'https://svinaymd.github.io/interactive-portfolio-xmb/',
+    //baseURL: 'http://127.0.0.1:8080',
+    baseURL: 'https://svinaymd.github.io/interactive-portfolio-xmb/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
