637ea2a309b505dcff668a696e971a82fbdd0142
Sebastian Vinay Muñoz Diaz <sebasvinaym@outlook.com>
2025-08-16 16:50:25 -0500

    testing playwright configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,22 +6,20 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
+  /* Maximum time one test can run for. */
+  timeout: process.env.CI ? 60 * 1000 : 30 * 1000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Use 2 workers on CI to run tests in parallel, speeding up the run. */
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    //baseURL: 'http://127.0.0.1:8080',
-    baseURL: 'https://svinaymd.github.io/interactive-portfolio-xmb/',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -65,9 +63,12 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm start',
+    url: 'http://127.0.0.1:8080',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000, // Give the server plenty of time to start
+    stdout: 'ignore',
+    stderr: 'pipe',
+  },
 });
